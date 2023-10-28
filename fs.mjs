efcc892451ea6a3f77a16515da0f7fa836c4de05
Misbahudin <misbahudin.id@gmail.com>
2023-10-28 20:01:54 +0700

    one line import fs

--- fs.mjs
+++ fs.mjs
@@ -1,9 +1,8 @@
-import { readFile } from 'fs/promises';
-import { writeFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 try {
    const filePath = new URL('./package.json', import.meta.url);
    const contents = await readFile(filePath, { encoding: 'utf8' });
-   await writeFile('temp.txt', 'Hello NodeJS');
+   await writeFile('temp.txt', 'Hello NodeJS ~~');
    console.log(contents);
 } catch (err) {
    console.error(err.message);
